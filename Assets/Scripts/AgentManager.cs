50fdb2a2980764b06199bf481dcf3597051f22e2
Bruna Borges <borgesbruna2510@gmail.com>
2022-05-13 21:44:06 -0300

    Comentarios

--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -8,17 +8,21 @@ public class AgentManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //Aqui o GameObject vai buscar por objetos com a tag ai 
         agents = GameObject.FindGameObjectsWithTag("ai");
     }
     // Update is called once per frame
     void Update()
     {
+        //Aqui diz que ao clicar com o botao principal do mouse o objeto ira fazer uma ação 
         if (Input.GetMouseButtonDown(0))
         {
+            //obtendo informações
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
                 foreach (GameObject a in agents)
+                    
                     a.GetComponent<AIControl>().agent.SetDestination(hit.point);
             }
         }
